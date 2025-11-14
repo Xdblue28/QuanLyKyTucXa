49d4e7f592d7e1b913c6ac83e4f289d6b74b9f62
AnBlue <xdblue28@gmail.com>
2025-11-14 22:47:07 +0700

    Day la phan dang nhap tren nhanh dang nhap

---
+++ DangNhap/Register/IRegisterCallback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Register
+{
+    public interface IRegisterCallback
+    {
+        void OnRegistered(string username);
+        void OnBackToLogin(); // thêm để xử lý nút "Back"
+    }
+}
